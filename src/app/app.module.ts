76ada7324473c9aa60df2e4dc213297f44ad5620
Artur Karczmarczyk <artur@ideaspot.pl>
2023-11-19 13:50:21 +0100

    adding tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TasksComponent } from './tasks/tasks.component';
 import {HttpClientModule} from "@angular/common/http";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import {HttpClientModule} from "@angular/common/http";
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,6 +10,7 @@ import {Task} from "../task";
 export class TasksComponent implements OnInit {
   public tasks: Task[] = [];
   public newTask: Task = {};
+  public isProcessing = false;
 
   constructor(
     private tasksService: TasksService
@@ -19,6 +20,23 @@ export class TasksComponent implements OnInit {
   ngOnInit() {
     this.tasksService.index().subscribe((tasks) => {
       this.tasks = tasks;
+    });
+    this.isProcessing = false;
+  }
+
+  addTask() {
+    if (this.newTask.title === undefined) {
+      return;
+    }
+
+    this.newTask.completed = false;
+    this.newTask.archived = false;
+
+    this.tasks.unshift(this.newTask); // optimistic update; try commenting this line off and compare the difference
+
+    this.tasksService.post(this.newTask).subscribe((task) => {
+      this.newTask = {};
+      this.ngOnInit();
     });
   }
 }
